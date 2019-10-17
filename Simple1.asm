b82c9b75dfef33554e19df7aae47b6965947a2fe
sc8017 <sc8017@skbl-422-008.ph.ic.ac.uk>
2019-10-17 15:48:47 +0100

    Serial Output

--- Simple1.asm
+++ Simple1.asm
@@ -8,39 +8,25 @@
 	
 start
 	
-	;movlw	0xff		    ; all bits in
-	;movwf	TRISD, A	    ; Port D Direction Register
-	;bsf	PADCFG1, RDPU, A	    ; Turn on pull-ups for Port D
-	;Setting port D as outup only
-	movlw 	0x0
-	movwf	TRISD, ACCESS	    ; Port D all outputs
-	
-	clrf	TRISC
-	clrf	TRISH
-	
-	banksel PADCFG1 ; PADCFG1 is not in Access Bank!! 
-	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on 
-	movlb	0x00 ; set BSR back to Bank 0 
-	setf	TRISE ; Tri-state PortE
-
 	movlw	0x00
 	movwf	0x20, ACCESS
 	
 	movlw	0x00
 	movwf	0x21, ACCESS
 	
-	movlw	0x04
+	movlw	0xf0
 	movwf	0x31, ACCESS ; num stored for the delay itself
 	movwf	0x30, ACCESS ; num stored to update the loop counter
 	
-	movlw	0x01
-	movwf	PORTD, ACCESS
+	call SPI_MasterInit
 	
-	;call	write1
-	call	read1
+	movlw 0x07
+	call SPI_MasterTransmit
 	
-	;call	write2
-	;call	read2
+	call delay
+	
+	movlw 0x01
+	call SPI_MasterTransmit
 	
 endprogram
 	nop
@@ -50,8 +36,6 @@ delay	call	sdelay
 	decfsz	0x31 ; decrement until zero 
 	bra	delay
 	movff	0x30, 0x31
-	;movlw	0x02
-	;movwf	0x31
 	
 	return
 	
@@ -64,48 +48,25 @@ ssdelay decfsz	0x20 ; decrement until zero
 	bra	ssdelay
 	return
 	
-write1	movlw	.17
-	movwf	PORTD, ACCESS ; OE1 high, OE2 high, rest low
-	clrf	TRISE
-	movlw	.3 ;  just a number to write to 1
-	movwf	PORTE, ACCESS
-	call	sdelay
-	movlw	.19 ; OE1 high, OE2 high, Clock1 high
-	movwf	PORTD, ACCESS
-	call	sdelay
-	setf	TRISE
 	
-	return
+
+SPI_MasterInit ; Set Clock edge to negative
+    bcf SSP2STAT, CKE
+    ; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+    movlw (1<<SSPEN)|(1<<CKP)|(0x02)
+    movwf SSP2CON1
+    ; SDO2 output; SCK2 output
+    bcf TRISD, SDO2
+    bcf TRISD, SCK2
+    return
+    
+SPI_MasterTransmit ; Start transmission of data (held in W)
+    movwf SSP2BUF
+    
+Wait_Transmit ; Wait for transmission to complete
+    btfss PIR2, SSP2IF
+    bra Wait_Transmit
+    bcf PIR2, SSP2IF ; clear interrupt flag
+    return
 	
-read1	movlw	.16
-	movwf	PORTD, ACCESS ; OE2 high, rest low
-	setf	TRISE
-	movf	PORTE, W
-	movwf	PORTH
-	call	sdelay
-	
-write2	movlw	.17
-	movwf	PORTD, ACCESS ; OE1 high, OE2 high, rest low
-	clrf	TRISE
-	movlw	.13 ;  just a number to write to 2
-	movwf	PORTE, ACCESS
-	call	sdelay
-	movlw	.25
-	movwf	PORTD, ACCESS ; OE1 high, OE2 high, clock2 high
-	call	sdelay
-	movlw	.16
-	movwf	PORTD, ACCESS ; OE2 high, rest low
-	setf	TRISE
-	
-	return
-	
-read2	movlw	.1
-	movwf	PORTD, ACCESS ; OE1 high, rest low
-	setf	TRISE
-	movf	PORTE, W
-	movwf	PORTC
-	call	sdelay
-	
-	return
-	
-	end
+    end
