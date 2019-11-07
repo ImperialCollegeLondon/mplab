a0f4837187f9ccbff7d14b1fc71dc9970e0ff19d
sc8017 <sc8017@skbl-422-008.ph.ic.ac.uk>
2019-11-07 16:11:32 +0000

    Drive the speaker with the sawtooth wave

--- sig_gen.asm
+++ sig_gen.asm
@@ -8,13 +8,18 @@ delay_count res 1   ; reserve one byte for counter in the delay routine
 int_hi	code	0x0008 ; high vector, no low vector
 	btfss	INTCON,TMR0IF ; check that this is timer0 interrupt
 	retfie	FAST ; if not then return
+	movlw	0x10
+	addwf	LATD, 1, 0
+	;incf	LATD ; increment PORTD
+	;incf	LATD ; increment PORTD
+	;incf	LATD ; increment PORTD
 	movlw	0x00
 	movwf	PORTE
-	incf	LATD ; increment PORTD
-	call	delay
+	
+	;call	delay
 	movlw	0x01
 	movwf	PORTE
-	call	delay
+	;call	delay
 	bcf	INTCON,TMR0IF ; clear interrupt flag
 	retfie	FAST ; fast return from interrupt
     
@@ -25,7 +30,7 @@ DAC_setup
 	clrf	TRISE
 	clrf	LATD ; Clear PORTD outputs
 	clrf	PORTE ; Clear PORTE outputs
-	movlw	b'10000100' ; Set timer0 to 16-bit, Fosc/4/256
+	movlw	b'10001000' ; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON ; = 500KHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE ; Enable timer0 interrupt
 	bsf	INTCON,GIE ; Enable all interrupts
