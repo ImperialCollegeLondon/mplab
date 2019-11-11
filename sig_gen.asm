b7162f6fc7221bd1c6beac044853322d42b5a1e1
sc8017 <sc8017@skbl-422-008.ph.ic.ac.uk>
2019-11-11 16:16:09 +0000

    Signal generated upon pressing RJ7, single frequency, using timer2

--- sig_gen.asm
+++ sig_gen.asm
@@ -6,40 +6,43 @@ acs0	udata_acs   ; reserve data space in access ram
 delay_count res 1   ; reserve one byte for counter in the delay routine
 	
 int_hi	code	0x0008 ; high vector, no low vector
-	btfss	INTCON,TMR0IF ; check that this is timer0 interrupt
+	btfss	PIR1,TMR2IF ; check that this is timer2 interrupt
 	retfie	FAST ; if not then return
 	;movlw	0x10
 	;addwf	LATD, 1, 0
 	incf	LATD ; increment PORTD
-	;incf	LATD ; increment PORTD
-	;incf	LATD ; increment PORTD
-	movlw	0x00
-	movwf	PORTE
+	;movlw	0x00
+	;movwf	PORTE
 	;call	delay
-	movlw	0x01
-	movwf	PORTE
+	;movlw	0x01
+	;movwf	PORTE
 	;call	delay
-	bcf	INTCON,TMR0IF ; clear interrupt flag
+	bcf	PIR1,TMR2IF ; clear timer2 interrupt flag
 	retfie	FAST ; fast return from interrupt
     
 DAC code
  
 DAC_setup
 	clrf	TRISD ; Set PORTD as all outputs
-	clrf	TRISE
+	;clrf	TRISE
 	clrf	LATD ; Clear PORTD outputs
-	clrf	PORTE ; Clear PORTE outputs
-	movlw	b'11001000' ; Set timer0 to 8-bit, Fosc/4/256
-	movwf	T0CON ; = 500KHz clock rate, approx 1sec rollover
-	bsf	INTCON,TMR0IE ; Enable timer0 interrupt
+	;clrf	PORTE ; Clear PORTE outputs -- dont need port e anymore
+	movlw	b'00000100' ; Set timer2 to 8-bit, prescaler 1:1, postscaler 1:1
+	movwf	T2CON ; = 500KHz clock rate, approx 1sec rollover
+	movlw	0x10
+	movwf	PR2
+	bsf	PIE1,TMR2IE ; Enable timer2 interrupt
 	bsf	INTCON,GIE ; Enable all interrupts
+	bsf     INTCON,PEIE ; enable peripheral interrupts
+	bsf	T2CON,TMR2ON      ; Start Timer2
 	return
 
-DAC_stop    
-	movlw	b'01001000' ; TURN TIMER0 OFF
-	movwf	T0CON 
-	bcf	INTCON,TMR0IE ; disable timer0 interrupt
-	bcf	INTCON,GIE ; disable all interrupts
+DAC_stop  
+	bcf	T2CON, TMR2ON ; turn off timer 2
+	;movlw	b'01001000' ; TURN TIMER0 OFF
+	;movwf	T0CON 
+	;bcf	INTCON,TMR0IE ; disable timer0 interrupt
+	;bcf	INTCON,GIE ; disable all interrupts
 	return
 	
 delay	decfsz	delay_count	; decrement until zero
