c1e5704d63498a55b9cc134abaa40f300ca3bc36
sc8017 <sc8017@skbl-422-008.ph.ic.ac.uk>
2019-11-11 17:16:38 +0000

    Signal generated upon pressing RJ7 and RJ6, two frequencies, using timer2

--- sig_gen.asm
+++ sig_gen.asm
@@ -1,7 +1,7 @@
 #include p18f87k22.inc
 	
 	
-	global	DAC_setup, DAC_stop
+	global	DAC_setup, DAC_stop, DAC_setup_2
 acs0	udata_acs   ; reserve data space in access ram
 delay_count res 1   ; reserve one byte for counter in the delay routine
 	
@@ -30,6 +30,21 @@ DAC_setup
 	movlw	b'00000100' ; Set timer2 to 8-bit, prescaler 1:1, postscaler 1:1
 	movwf	T2CON ; = 500KHz clock rate, approx 1sec rollover
 	movlw	0x10
+	movwf	PR2
+	bsf	PIE1,TMR2IE ; Enable timer2 interrupt
+	bsf	INTCON,GIE ; Enable all interrupts
+	bsf     INTCON,PEIE ; enable peripheral interrupts
+	bsf	T2CON,TMR2ON      ; Start Timer2
+	return
+
+DAC_setup_2
+	clrf	TRISD ; Set PORTD as all outputs
+	;clrf	TRISE
+	clrf	LATD ; Clear PORTD outputs
+	;clrf	PORTE ; Clear PORTE outputs -- dont need port e anymore
+	movlw	b'00000100' ; Set timer2 to 8-bit, prescaler 1:1, postscaler 1:1
+	movwf	T2CON ; = 500KHz clock rate, approx 1sec rollover
+	movlw	0x40
 	movwf	PR2
 	bsf	PIE1,TMR2IE ; Enable timer2 interrupt
 	bsf	INTCON,GIE ; Enable all interrupts
